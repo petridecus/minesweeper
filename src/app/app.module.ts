48fdbb99d9fdc0ab22fffad17b802fb8b232f1c3
petridecus <apetrides32@gmail.com>
2019-06-25 20:53:14 -0400

    starting point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
+import { SpaceComponent } from './components/space/space.component';
+import { HighlightedBlockComponent } from './components/highlighted-block/highlighted-block.component';
+import { StandardBlockComponent } from './components/standard-block/standard-block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    SpaceComponent,
+    HighlightedBlockComponent,
+    StandardBlockComponent
   ],
   imports: [
     BrowserModule,
